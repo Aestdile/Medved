b4af295e23ccb5799b4c7d0789ba60b8aa7a43fc
Aestdile <aestdile@gmail.com>
2024-10-19 10:13:13 +0500

    Basic 2

--- Basic 2/Program.cs
+++ Basic 2/Program.cs
@@ -1,1 +1,42 @@
 
+
+
+
+// Basic Task-2
+/*
+// task-1
+
+Console.Write("Enter a: ");
+string a=Console.ReadLine();
+int num1=Convert.ToInt32(a);
+Console.Write("Enter b: ");
+string b=Console.ReadLine();
+int num2=Convert.ToInt32(b);
+
+Console.Write("Amalni kiriting: +, -, *, /");
+string belgi=Console.ReadLine();
+Console.WriteLine($"{belgi=="+"}, a+b={num1+num2}");
+Console.WriteLine($"{belgi=="-"}, a-b={num1-num2}");
+Console.WriteLine($"{belgi=="*"}, a*b={num1*num2}");
+Console.WriteLine($"{belgi=="/"}, a/b={num1/num2}");
+
+
+// task-2
+
+Console.Write("N ni kiriting: ");
+string n=Console.ReadLine();
+int N=Convert.ToInt32(n);
+int sum=(N*(N+1))/2;
+Console.WriteLine($"Sum: {sum}");
+
+
+// task-3
+
+Console.Write("N ni kiriting: ");
+string n=Console.ReadLine();
+int N=Convert.ToInt32(n);
+Console.WriteLine($"Kiritilgan son juft va qoldiq={N%2==0}");
+Console.WriteLine($"Kiritilgan son toq va qoldiq={N%2!=0}");
+
+
+*/
